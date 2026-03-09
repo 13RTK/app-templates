f0674d4a079eb0bf9a8c53572cbeef2f88ba3741
alex <907785012@qq.com>
2026-03-09 20:21:49 +0800

    remove biome suppression

--- human-resource-management/react-version/src/main.tsx
+++ human-resource-management/react-version/src/main.tsx
@@ -1,8 +1,8 @@
-/** biome-ignore-all lint/style/noNonNullAssertion: <just template> */
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+
 import App from './App.tsx';
+import './index.css';
+import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
